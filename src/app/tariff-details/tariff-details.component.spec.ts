99b7c2ce6f2c44f948f3142a9eb4b0df29de2c31
Tonida Baraza <tonie@tonidas-mbp.localdomain>
2019-11-19 04:04:51 +0100

    Add tariff details component

---
+++ src/app/tariff-details/tariff-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TariffDetailsComponent } from './tariff-details.component';
+
+describe('TariffDetailsComponent', () => {
+  let component: TariffDetailsComponent;
+  let fixture: ComponentFixture<TariffDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TariffDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TariffDetailsComponent);
+    component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tariff-details/tariff-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tariff-details',
+  templateUrl: './tariff-details.component.html',
+  styleUrls: ['./tariff-details.component.sass']
+})
+export class TariffDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
