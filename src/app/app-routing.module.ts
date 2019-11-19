76ea34185257522addce20b644b2a3909e024674
Tonida Baraza <tonie@tonidas-mbp.localdomain>
2019-11-19 04:03:29 +0100

    Initial app set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TariffListComponent } from './tariff-list/tariff-list.component';
+import { TariffDetailsComponent } from './tariff-details/tariff-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TariffListComponent },
+  { path: 'tariffs/:tariffId', component: TariffDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TariffListComponent } from './tariff-list/tariff-list.component';
+import { TariffDetailsComponent } from './tariff-details/tariff-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TariffListComponent,
+    TariffDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
